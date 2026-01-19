93ec45ede11785ec8652e180507d1571c9f6510b
thedhanawada <wep.proto@gmail.com>
2026-01-19 20:58:02 +1100

    Fix Vercel build - use native Next.js

--- www/next.config.js
+++ www/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  // Let Vercel handle the build natively
 };
 
 module.exports = nextConfig;
